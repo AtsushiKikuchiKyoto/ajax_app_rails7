7f2d6c580d736f2583dd4afaffd6cccf6bd9b3a9
AtsushiKikuchiKyoto <tosa81st@gmail.com>
2024-10-06 18:24:18 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,5 +1,19 @@
 //Ajax
 
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時:${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>  `;
+  return html;
+};
+
 function post(){
   const form = document.getElementById("form");
   form.addEventListener("submit", (e) => {
@@ -11,8 +25,20 @@ function post(){
     XHR.open("POST", "/posts", true); //非同期true
     XHR.responseType = "json";
     XHR.send(formData);
-    console.log("aaaaa");
-
+    XHR.onload = () => { //受信した時の処理
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      }; //HTTPステータスコード200番は「成功」,他はエラー表示
+      const list     = document.getElementById("list");
+      const formText = document.getElementById("content");
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+        // beforebegin	要素の直前
+        // afterbegin	要素内部の、最初の子要素の直前
+        // beforeend	要素内部の、最後の子要素の直後
+        // afterend	要素の直後
+      formText.value = "";
+      }; 
   })    
 };
 
